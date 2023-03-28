2f451b4fdc1bb224c507bb3be1b84edcb72fb067
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-03-28 22:04:10 +0530

    qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,12 @@
+# config.source('qutebrowser-themes/themes/onedark.py')
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
